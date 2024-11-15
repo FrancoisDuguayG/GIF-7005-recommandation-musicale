737a95baaad0bd1d3351df70f8948fc187b9167e
FrancoisDuguayG <francois.duguay@cegeplimoilou.ca>
2024-11-14 19:16:02 -0500

    Add data

--- read_user_data.py
+++ read_user_data.py
@@ -4,7 +4,7 @@ from scipy.sparse import save_npz, load_npz, coo_array
 
 
 # Charger les données
-file_path = Path('./data/train_triplets_small.txt')
+file_path = Path('data/user_taste_profile_small.txt')
 data = pd.read_csv(file_path, sep='\t', header=None, names=['user_id', 'song_id', 'play_count'])
 
 # mapper les IDs à des index uniques
@@ -15,7 +15,7 @@ song_ids = data['song_id'].astype('category').cat.codes
 sparse_matrix = coo_array((data['play_count'], (user_ids, song_ids)))
 
 # Sauvegarder la matrice
-save_npz(Path('./data/user_song_matrix.npz'), sparse_matrix)
+save_npz(Path('process_data/user_song_matrix.npz'), sparse_matrix)
 
-matrix = load_npz(Path('./data/user_song_matrix.npz'))
+matrix = load_npz(Path('process_data/user_song_matrix.npz'))
 
